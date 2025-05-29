c640192f58c60a509a76e15d63668218e0ca0b89
FineFindus <FineFindus@proton.me>
2025-05-29 21:57:14 +0200

    build: use custom app id suffix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.metrolist.music"
+        applicationId = "com.metrolist.music.custom"
         minSdk = 26
         targetSdk = 36
         versionCode = 129
