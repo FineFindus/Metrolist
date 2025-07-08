OuterTune/OuterTune

ui: display `musicResponsiveListItemFlexColumnRenderer` items in HomeScreen
YouTube Music uses a song grid for some suggestions (especially when
logged in) such as 'Trending in shorts', 'Covers', etc.
Previously these were ignored, as the HomePage was only able to parse
list sections. This expands the support for parsing and displaying the
sections on the home screen.

innertube/src/main/java/com/zionhuang/innertube/pages/HomePage.kt
@@ -5,6 +5,7 @@ import com.zionhuang.innertube.models.Artist
 import com.zionhuang.innertube.models.ArtistItem
 import com.zionhuang.innertube.models.BrowseEndpoint
 import com.zionhuang.innertube.models.MusicCarouselShelfRenderer
+import com.zionhuang.innertube.models.MusicResponsiveListItemRenderer
 import com.zionhuang.innertube.models.MusicTwoRowItemRenderer
 import com.zionhuang.innertube.models.PlaylistItem
 import com.zionhuang.innertube.models.SongItem
@@ -20,6 +21,7 @@ data class HomePage(
         val thumbnail: String?,
         val endpoint: BrowseEndpoint?,
         val items: List<YTItem>,
+        val sectionType: SectionType,
     ) {
         companion object {
             fun fromMusicCarouselShelfRenderer(renderer: MusicCarouselShelfRenderer): Section? {
@@ -29,12 +31,19 @@ data class HomePage(
                     thumbnail = renderer.header.musicCarouselShelfBasicHeaderRenderer.thumbnail?.musicThumbnailRenderer?.getThumbnailUrl(),
                     endpoint = renderer.header.musicCarouselShelfBasicHeaderRenderer.moreContentButton?.buttonRenderer?.navigationEndpoint?.browseEndpoint,
                     items = renderer.contents.mapNotNull {
-                        it.musicTwoRowItemRenderer
-                    }.mapNotNull {
-                        fromMusicTwoRowItemRenderer(it)
+                        it.musicTwoRowItemRenderer?.let { renderer ->
+                            fromMusicTwoRowItemRenderer(
+                                renderer
+                            )
+                        } ?: it.musicResponsiveListItemRenderer?.let { renderer ->
+                            fromMusicResponsiveListItemRenderer(
+                                renderer
+                            )
+                        }
                     }.ifEmpty {
                         return null
-                    }
+                    },
+                    sectionType = if (renderer.contents.any { it.musicResponsiveListItemRenderer != null }) SectionType.GRID else SectionType.LIST,
                 )
             }
 
@@ -121,7 +130,36 @@ data class HomePage(
                     else -> null
                 }
             }
+
+            private fun fromMusicResponsiveListItemRenderer(renderer: MusicResponsiveListItemRenderer): YTItem? {
+                return when {
+                    renderer.isSong -> {
+                        SongItem(
+                            id = renderer.playlistItemData?.videoId ?: return null,
+                            title = renderer.flexColumns.firstOrNull()?.musicResponsiveListItemFlexColumnRenderer?.text?.runs?.firstOrNull()?.text?: return null,
+                            artists = listOfNotNull(renderer.flexColumns.getOrNull(1)?.musicResponsiveListItemFlexColumnRenderer?.text?.runs?.firstOrNull()?.let {
+                                Artist(
+                                    name = it.text,
+                                    id = it.navigationEndpoint?.browseEndpoint?.browseId
+                                )
+                            }),
+                            album = null,
+                            duration = null,
+                            thumbnail = renderer.thumbnail?.musicThumbnailRenderer?.getThumbnailUrl() ?: return null,
+                            explicit = renderer.badges?.find {
+                                it.musicInlineBadgeRenderer?.icon?.iconType == "MUSIC_EXPLICIT_BADGE"
+                            } != null
+                        )
+                    }
+                    // the home page only displays Songs as MusicResponsiveListItemRenderer
+                    else -> null
+                }
+            }
         }
+    }
+
+    enum class SectionType {
+        LIST, GRID
     }
 
 //    fun filterExplicit(enabled: Boolean = true) =

FineFindus/Metrolist

innertube/src/main/kotlin/com/metrolist/innertube/pages/HomePage.kt
@@ -5,6 +5,7 @@ import com.metrolist.innertube.models.Artist
 import com.metrolist.innertube.models.ArtistItem
 import com.metrolist.innertube.models.BrowseEndpoint
 import com.metrolist.innertube.models.MusicCarouselShelfRenderer
+import com.metrolist.innertube.models.MusicResponsiveListItemRenderer
 import com.metrolist.innertube.models.MusicTwoRowItemRenderer
 import com.metrolist.innertube.models.PlaylistItem
 import com.metrolist.innertube.models.SectionListRenderer
@@ -40,6 +41,7 @@ data class HomePage(
         val thumbnail: String?,
         val endpoint: BrowseEndpoint?,
         val items: List<YTItem>,
+        val sectionType: SectionType,
     ) {
         companion object {
             fun fromMusicCarouselShelfRenderer(renderer: MusicCarouselShelfRenderer): Section? {
@@ -49,12 +51,19 @@ data class HomePage(
                     thumbnail = renderer.header.musicCarouselShelfBasicHeaderRenderer.thumbnail?.musicThumbnailRenderer?.getThumbnailUrl(),
                     endpoint = renderer.header.musicCarouselShelfBasicHeaderRenderer.moreContentButton?.buttonRenderer?.navigationEndpoint?.browseEndpoint,
                     items = renderer.contents.mapNotNull {
-                        it.musicTwoRowItemRenderer
-                    }.mapNotNull {
-                        fromMusicTwoRowItemRenderer(it)
+                        it.musicTwoRowItemRenderer?.let { renderer ->
+                            fromMusicTwoRowItemRenderer(
+                                renderer
+                            )
+                        } ?: it.musicResponsiveListItemRenderer?.let { renderer ->
+                            fromMusicResponsiveListItemRenderer(
+                                renderer
+                            )
+                        }
                     }.ifEmpty {
                         return null
-                    }
+                    },
+                    sectionType = if (renderer.contents.any { it.musicResponsiveListItemRenderer != null }) SectionType.GRID else SectionType.LIST,
                 )
             }
 
@@ -141,6 +150,31 @@ data class HomePage(
                     else -> null
                 }
             }
+
+            private fun fromMusicResponsiveListItemRenderer(renderer: MusicResponsiveListItemRenderer): YTItem? {
+                return when {
+                    renderer.isSong -> {
+                        SongItem(
+                            id = renderer.playlistItemData?.videoId ?: return null,
+                            title = renderer.flexColumns.firstOrNull()?.musicResponsiveListItemFlexColumnRenderer?.text?.runs?.firstOrNull()?.text?: return null,
+                            artists = listOfNotNull(renderer.flexColumns.getOrNull(1)?.musicResponsiveListItemFlexColumnRenderer?.text?.runs?.firstOrNull()?.let {
+                                Artist(
+                                    name = it.text,
+                                    id = it.navigationEndpoint?.browseEndpoint?.browseId
+                                )
+                            }),
+                            album = null,
+                            duration = null,
+                            thumbnail = renderer.thumbnail?.musicThumbnailRenderer?.getThumbnailUrl() ?: return null,
+                            explicit = renderer.badges?.find {
+                                it.musicInlineBadgeRenderer?.icon?.iconType == "MUSIC_EXPLICIT_BADGE"
+                            } != null
+                        )
+                    }
+                    // the home page only displays Songs as MusicResponsiveListItemRenderer
+                    else -> null
+                }
+            }
         }
     }
 
@@ -151,4 +185,7 @@ data class HomePage(
             })
         } else this
 
+    enum class SectionType {
+        LIST, GRID
+    }
 }